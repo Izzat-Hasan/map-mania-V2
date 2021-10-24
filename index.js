dca46c9fef878ef2e78819c4c7d390435fa5f012
Izzat-Hasan <izzathasan03@gmail.com>
2021-10-23 20:43:46 -0500

    updated interface

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ function nextPlace() {
 function cheatcode(){
    score = 10;
   document.getElementById("score-id").value = score;
+  document.getElementById("hint").value = "you won!";
   var favLocations = [
       ['Mackinac Island', 45.842829962, -84.617997528],
       ['San Diego', 32.7157, -117.1611],
